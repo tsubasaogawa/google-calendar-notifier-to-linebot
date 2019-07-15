a78c366901a6aeec0d390b4bb85e499667baf754
Tsubasa Ogawa <hurikake@gmail.com>
2019-07-15 10:24:35 +0000

    make it possible to choose items by visibility and date range

--- cal.go
+++ cal.go
@@ -17,6 +17,11 @@ type Cal struct{
 	Plans []Plan
 }
 
+const (
+	// format
+	dtfmt = "2006-01-02T00:00:00.000Z"
+)
+
 func (c *Cal) NewCal(cred string, maxResults int64) {
 	(*c).cred = cred
 	(*c).maxResults = maxResults
@@ -27,45 +32,52 @@ func (c *Cal) NewCal(cred string, maxResults int64) {
 func (c *Cal) createGoogleCalendar() {
 	b, err := ioutil.ReadFile((*c).cred)
 	if err != nil {
-			log.Fatalf("Unable to read client secret file: %v", err)
+		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
 	if err != nil {
-			log.Fatalf("Unable to parse client secret file to config: %v", err)
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 
 	client := Client{}.Create(config)
 	srv, err := calendar.New(client)
 	if err != nil {
-			log.Fatalf("Unable to retrieve Calendar client: %v", err)
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
 	}
 
 	(*c).srv = srv
 }
 
-func (c *Cal) Retrieve() {
-	t := time.Now().Format(time.RFC3339)
+func (c *Cal) Retrieve(days int, onlyPubItem bool) {
+	now := time.Now()
+	st := now.Format(dtfmt)
+	ed := now.AddDate(0, 0, days).Format(dtfmt)
 	events, err := c.srv.Events.List("primary").ShowDeleted(false).
-			SingleEvents(true).TimeMin(t).MaxResults((*c).maxResults).OrderBy("startTime").Do()
+		SingleEvents(true).TimeMin(st).TimeMax(ed).MaxResults((*c).maxResults).OrderBy("startTime").Do()
 	if err != nil {
-			log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+		log.Fatalf("Unable to retrieve next %d of the user's events: %v", (*c).maxResults, err)
 	}
 
 	if len(events.Items) == 0 {
-			fmt.Println("No upcoming events found.")
+		fmt.Println("No upcoming events found.")
 	} else {
-			for _, item := range events.Items {
-					date := item.Start.DateTime
-					if date == "" {
-							date = item.Start.Date
-					}
-					// fmt.Printf("%v (%v)\n", item.Summary, date)
-					(*c).Plans = append((*c).Plans, Plan{
-						date: date,
-						title: item.Summary,
-					})
+		for _, item := range events.Items {
+			if onlyPubItem && item.Visibility == "private" {
+				continue
 			}
+
+			date := item.Start.DateTime
+			if date == "" {
+				date = item.Start.Date
+			}
+
+			(*c).Plans = append((*c).Plans, Plan{
+				date: date,
+				title: item.Summary,
+			})
+			fmt.Printf("%+v\n\n", item)
+		}
 	}
 }
--- notifier.go
+++ notifier.go
@@ -10,12 +10,17 @@ const (
 
 	// maxResults is the maximum number of results.
 	maxResults = 10
+
+	// days
+	days = 7
+
+	onlyPubItem = true
 )
 
 func main() {
 	cal := &Cal{}
 	cal.NewCal(cred, maxResults)
-	cal.Retrieve()
+	cal.Retrieve(days, onlyPubItem)
 
 	fmt.Printf("%v", (*cal).Plans)
 }
